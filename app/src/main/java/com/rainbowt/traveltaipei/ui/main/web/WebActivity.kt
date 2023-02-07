22447c7fea530cf221fd8c2c3f647b2749816b47
RainBowT <rainbowt@playmore.com.hk>
2023-02-08 01:57:09 +0800

    [feat] add web page

---
+++ app/src/main/java/com/rainbowt/traveltaipei/ui/main/web/WebActivity.kt
@@ -0,0 +1,12 @@
+package com.rainbowt.traveltaipei.ui.main.web
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rainbowt.traveltaipei.R
+
+class WebActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web)
+    }
+}
---
+++ app/src/main/java/com/rainbowt/traveltaipei/ui/main/web/WebFragment.kt
@@ -0,0 +1,45 @@
+package com.rainbowt.traveltaipei.ui.main.web
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.rainbowt.traveltaipei.Constants.GOOGLE_URL
+import com.rainbowt.traveltaipei.R
+import com.rainbowt.traveltaipei.databinding.FragmentWebBinding
+
+class WebFragment : Fragment() {
+
+    private var url: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            url = it.getString(URL)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentWebBinding.inflate(LayoutInflater.from(context))
+        binding.webview.loadUrl(if (url == null) GOOGLE_URL else url!!)
+        return binding.root
+    }
+
+    companion object {
+        const val URL = "url"
+
+        @JvmStatic
+        fun newInstance(url: String?) =
+            WebFragment().apply {
+                arguments = Bundle().apply {
+                    putString(URL, url)
+                }
+            }
+    }
+}
